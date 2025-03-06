1376889d2341730140d0e3bee5faeb971828e0a9
tsztin0217 <tsztin0217@gmail.com>
2025-03-05 21:29:39 -0500

    added updateCustomer function

--- app.js
+++ app.js
@@ -41,26 +41,52 @@ const menu = async () => {
         case "4":
             await deleteCustomer();
         case "5":
-    };
+    }
 }
 
 
 
 // const username = prompt('What is your name? ');
 
-// const createCustomer = async () => {
-//     const name = prompt("Enter customer name: ");
-//     const age = parseInt(prompt("Enter customer age: "))
+const createCustomer = async () => {
+    const name = prompt("Enter customer name: ");
+    const age = parseInt(prompt("Enter customer age: "))
 
-//     const customerData = {
-//         name: name,
-//         age: age
-//     };
-//     console.log(``)
+    const customerData = {
+        name: name,
+        age: age
+    };
+    const customer = await Customer.create(customerData);
+    console.log();
 
-// }
+}
+
+const viewCustomer = async () => {
+    const customers = await Customer.find({});
+    console.log(customers);
+}
 
 
+const updateCustomer = async () => {
+    const customers = await Customer.find({});
+    console.log(`Below is a list of of customers:
+        ${customers} 
+        `)
+    const askId = prompt("Copy and paste the id of the customer you would like to update here: ")
+    const updatedCustomer = await Customer.findById(askId);
+    console.log(`# user inputs ${askId}`);
+
+    const newName = prompt("What is the customers new name?");
+    console.log(`# user inputs ${newName}`);
+    updatedCustomer.name = newName;
+
+    const newAge = prompt("What is the customers new age?");
+    updatedCustomer.age = newAge;
+    console.log(`# user inputs ${newAge}`);
+
+    await updatedCustomer.save();
+
+}
 // console.log(`# user inputs {choice}`)
 
 // console.log(`Your name is ${username}`);
